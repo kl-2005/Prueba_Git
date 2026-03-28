e99c16cd8b76a81f6d486d37d78b821f1ac0eae9
Gabriel <gabrielbonilla561@gmail.com>
2026-03-28 15:01:41 -0500

    cambios en clase tutorias

--- Prueba_Git/Tutorias.cs
+++ Prueba_Git/Tutorias.cs
@@ -12,6 +12,17 @@ namespace Prueba_Git
         public int Id{ get; set; }
         public int Cupos{ get; set; }
         public List<Estudiante> ListaEstudiantes{ get; set; }
-        
+        public Tutor Tutor{ get; set; }
+        public Tutorias()
+        {
+
+        }
+        public Tutorias(int id, int cupos, List<Estudiante> listaEstudiantes, Tutor tutor)
+        {
+            Id = id;
+            Cupos = cupos;
+            ListaEstudiantes = listaEstudiantes;
+            Tutor = tutor;
+        }
     }
 }
