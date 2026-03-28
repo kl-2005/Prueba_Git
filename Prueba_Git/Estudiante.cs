b58f4b10bac16b0a2de520d26ecbe0148bf2ca80
Gabriel <gabrielbonilla561@gmail.com>
2026-03-28 18:19:31 -0500

    cmb

--- Prueba_Git/Estudiante.cs
+++ Prueba_Git/Estudiante.cs
@@ -11,7 +11,6 @@ namespace Prueba_Git
         public string Cedula { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        public string Correo { get; set; }
         public Tutor Tutor { get; set; }
         public Horario Horario { get; set; }
 
