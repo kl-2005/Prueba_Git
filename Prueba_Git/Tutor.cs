c04af97ff828302c1cc093477413ee7574472561
Gabriel <gabrielbonilla561@gmail.com>
2026-03-28 11:57:17 -0500

    propiedades y constructores tutor

---
+++ Prueba_Git/Tutor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Git
+{
+    internal class Tutor
+    {
+        public Tutor()
+        {
+            
+        }
+
+        public Tutor(string cedula, string nombre, string asignatura)
+        {
+            Cedula = cedula;
+            Nombre = nombre;
+            Asignatura = asignatura;
+        }
+
+        public String Cedula{ get; set; }
+        public String Nombre{ get; set; }
+        public String Asignatura{ get; set; }
+    }
+}
