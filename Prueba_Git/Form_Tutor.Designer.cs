96b4449c5c81d44f7e90f7abe945c638969eac2a
Odalis Caiza <ocaiza1258@uta.edu.ec>
2026-03-28 22:14:39 -0500

    vfefed

--- Prueba_Git/Form_Tutor.Designer.cs
+++ Prueba_Git/Form_Tutor.Designer.cs
@@ -54,40 +54,35 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(16, 20);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Location = new System.Drawing.Point(24, 31);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(509, 424);
+            this.groupBox1.Size = new System.Drawing.Size(572, 652);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Tutor";
             // 
             // numericUpDown_CapacidadMaxima
             // 
-            this.numericUpDown_CapacidadMaxima.Location = new System.Drawing.Point(127, 114);
-            this.numericUpDown_CapacidadMaxima.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown_CapacidadMaxima.Location = new System.Drawing.Point(190, 175);
             this.numericUpDown_CapacidadMaxima.Name = "numericUpDown_CapacidadMaxima";
-            this.numericUpDown_CapacidadMaxima.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDown_CapacidadMaxima.Size = new System.Drawing.Size(120, 26);
             this.numericUpDown_CapacidadMaxima.TabIndex = 13;
             // 
             // dataGridView_Tutor
             // 
             this.dataGridView_Tutor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_Tutor.Location = new System.Drawing.Point(17, 177);
-            this.dataGridView_Tutor.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView_Tutor.Location = new System.Drawing.Point(26, 272);
             this.dataGridView_Tutor.Name = "dataGridView_Tutor";
             this.dataGridView_Tutor.RowHeadersWidth = 62;
             this.dataGridView_Tutor.RowTemplate.Height = 28;
-            this.dataGridView_Tutor.Size = new System.Drawing.Size(338, 227);
+            this.dataGridView_Tutor.Size = new System.Drawing.Size(507, 349);
             this.dataGridView_Tutor.TabIndex = 8;
             // 
             // button_RegistrarTutor
             // 
-            this.button_RegistrarTutor.Location = new System.Drawing.Point(253, 109);
-            this.button_RegistrarTutor.Margin = new System.Windows.Forms.Padding(2);
+            this.button_RegistrarTutor.Location = new System.Drawing.Point(380, 168);
             this.button_RegistrarTutor.Name = "button_RegistrarTutor";
-            this.button_RegistrarTutor.Size = new System.Drawing.Size(67, 29);
+            this.button_RegistrarTutor.Size = new System.Drawing.Size(100, 45);
             this.button_RegistrarTutor.TabIndex = 7;
             this.button_RegistrarTutor.Text = "Registrar";
             this.button_RegistrarTutor.UseVisualStyleBackColor = true;
@@ -96,66 +91,59 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(14, 117);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(21, 180);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 26);
+            this.label4.Size = new System.Drawing.Size(143, 40);
             this.label4.TabIndex = 6;
             this.label4.Text = "Capacidad maxima\r\n\r\n";
             // 
             // textBox_NombreTutor
             // 
-            this.textBox_NombreTutor.Location = new System.Drawing.Point(203, 69);
-            this.textBox_NombreTutor.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_NombreTutor.Location = new System.Drawing.Point(304, 106);
             this.textBox_NombreTutor.Name = "textBox_NombreTutor";
-            this.textBox_NombreTutor.Size = new System.Drawing.Size(139, 20);
+            this.textBox_NombreTutor.Size = new System.Drawing.Size(206, 26);
             this.textBox_NombreTutor.TabIndex = 4;
             // 
             // textBox_CedulaTutor
             // 
-            this.textBox_CedulaTutor.Location = new System.Drawing.Point(29, 69);
-            this.textBox_CedulaTutor.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_CedulaTutor.Location = new System.Drawing.Point(44, 106);
             this.textBox_CedulaTutor.Name = "textBox_CedulaTutor";
-            this.textBox_CedulaTutor.Size = new System.Drawing.Size(135, 20);
+            this.textBox_CedulaTutor.Size = new System.Drawing.Size(200, 26);
             this.textBox_CedulaTutor.TabIndex = 3;
             this.textBox_CedulaTutor.TextChanged += new System.EventHandler(this.textBox_CedulaTutor_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(250, 40);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(375, 62);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(73, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Nombres\r\n";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 40);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(68, 62);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.Size = new System.Drawing.Size(59, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Cedula";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(185, 31);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(278, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 20);
             this.label1.TabIndex = 0;
             // 
             // Form_Tutor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(418, 451);
+            this.ClientSize = new System.Drawing.Size(627, 694);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form_Tutor";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
