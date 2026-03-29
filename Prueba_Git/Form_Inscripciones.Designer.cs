613357d2cbf899d7e2201c31a511b7cfc9951ab0
Odalis Caiza <ocaiza1258@uta.edu.ec>
2026-03-29 15:23:50 -0500

    ll

--- Prueba_Git/Form_Inscripciones.Designer.cs
+++ Prueba_Git/Form_Inscripciones.Designer.cs
@@ -65,9 +65,9 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Maiandra GD", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(40, 24);
+            this.groupBox1.Location = new System.Drawing.Point(29, 24);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(788, 255);
+            this.groupBox1.Size = new System.Drawing.Size(659, 255);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "DATOS DE LA INSCRIPCION";
@@ -77,9 +77,9 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::Prueba_Git.Properties.Resources.scholarship;
-            this.pictureBox2.Location = new System.Drawing.Point(563, 28);
+            this.pictureBox2.Location = new System.Drawing.Point(483, 40);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(186, 211);
+            this.pictureBox2.Size = new System.Drawing.Size(161, 188);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 16;
             this.pictureBox2.TabStop = false;
@@ -87,7 +87,7 @@
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(243, 165);
+            this.comboBox3.Location = new System.Drawing.Point(201, 165);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(260, 29);
             this.comboBox3.TabIndex = 15;
@@ -96,7 +96,7 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(243, 114);
+            this.comboBox2.Location = new System.Drawing.Point(201, 114);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(260, 29);
             this.comboBox2.TabIndex = 14;
@@ -104,7 +104,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(243, 63);
+            this.comboBox1.Location = new System.Drawing.Point(201, 63);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(260, 29);
             this.comboBox1.TabIndex = 13;
@@ -113,7 +113,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(66, 165);
+            this.label3.Location = new System.Drawing.Point(24, 165);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(106, 21);
             this.label3.TabIndex = 3;
@@ -122,7 +122,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(66, 114);
+            this.label2.Location = new System.Drawing.Point(24, 114);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(84, 21);
             this.label2.TabIndex = 2;
@@ -131,7 +131,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 63);
+            this.label1.Location = new System.Drawing.Point(24, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(127, 21);
             this.label1.TabIndex = 1;
@@ -140,7 +140,7 @@
             // button_Inscribir
             // 
             this.button_Inscribir.ForeColor = System.Drawing.Color.Black;
-            this.button_Inscribir.Location = new System.Drawing.Point(935, 248);
+            this.button_Inscribir.Location = new System.Drawing.Point(741, 245);
             this.button_Inscribir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button_Inscribir.Name = "button_Inscribir";
             this.button_Inscribir.Size = new System.Drawing.Size(118, 36);
@@ -156,14 +156,14 @@
             this.dataGridView_Inscripciones.Name = "dataGridView_Inscripciones";
             this.dataGridView_Inscripciones.RowHeadersWidth = 62;
             this.dataGridView_Inscripciones.RowTemplate.Height = 28;
-            this.dataGridView_Inscripciones.Size = new System.Drawing.Size(493, 244);
+            this.dataGridView_Inscripciones.Size = new System.Drawing.Size(377, 244);
             this.dataGridView_Inscripciones.TabIndex = 11;
             this.dataGridView_Inscripciones.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(941, 29);
+            this.label4.Location = new System.Drawing.Point(746, 26);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(102, 21);
             this.label4.TabIndex = 9;
@@ -176,7 +176,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(509, 244);
+            this.dataGridView1.Size = new System.Drawing.Size(390, 244);
             this.dataGridView1.TabIndex = 7;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -187,7 +187,7 @@
             this.groupBox2.ForeColor = System.Drawing.Color.Black;
             this.groupBox2.Location = new System.Drawing.Point(12, 306);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(562, 328);
+            this.groupBox2.Size = new System.Drawing.Size(440, 328);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "LISTA DE INSCRIPCIONES REALIZADAS";
@@ -198,9 +198,9 @@
             this.groupBox3.Controls.Add(this.dataGridView_Inscripciones);
             this.groupBox3.Font = new System.Drawing.Font("Maiandra GD", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox3.ForeColor = System.Drawing.Color.Black;
-            this.groupBox3.Location = new System.Drawing.Point(571, 306);
+            this.groupBox3.Location = new System.Drawing.Point(444, 306);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(547, 328);
+            this.groupBox3.Size = new System.Drawing.Size(460, 328);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "RESUMEN DE CUPOS POR TUTOR";
@@ -209,7 +209,7 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::Prueba_Git.Properties.Resources.checklist;
-            this.pictureBox1.Location = new System.Drawing.Point(914, 71);
+            this.pictureBox1.Location = new System.Drawing.Point(732, 77);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(153, 160);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -221,10 +221,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MistyRose;
-            this.ClientSize = new System.Drawing.Size(1130, 630);
+            this.ClientSize = new System.Drawing.Size(916, 630);
             this.Controls.Add(this.button_Inscribir);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
