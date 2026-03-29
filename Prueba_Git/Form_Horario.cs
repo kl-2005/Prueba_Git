89a3bb8ddfb978702acd276a9363e2246ad78315
Gabriel <gabrielbonilla561@gmail.com>
2026-03-28 22:29:51 -0500

    cmb

--- Prueba_Git/Form_Horario.cs
+++ Prueba_Git/Form_Horario.cs
@@ -12,9 +12,16 @@ namespace Prueba_Git
 {
     public partial class Form_Horario : Form
     {
+        
         public Form_Horario()
         {
             InitializeComponent();
+            CargarDiasInscripcion();
+        }
+
+        private void CargarDiasInscripcion()
+        {
+            comboBox_Dia.Items.Add(new String[] { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes" });
         }
 
         private void label1_Click(object sender, EventArgs e)
