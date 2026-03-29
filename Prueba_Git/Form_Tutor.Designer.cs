bb3590c7a8f15d8a1aec8ff07c5ff330ed1c97b8
Odalis Caiza <ocaiza1258@uta.edu.ec>
2026-03-29 13:51:43 -0500

    DW

--- Prueba_Git/Form_Tutor.Designer.cs
+++ Prueba_Git/Form_Tutor.Designer.cs
@@ -44,6 +44,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView_Tutor = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_CapacidadMaxima)).BeginInit();
@@ -54,6 +56,8 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.IndianRed;
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Controls.Add(this.textBox_Buscar);
             this.groupBox1.Controls.Add(this.button_Buscar);
@@ -70,7 +74,7 @@
             this.groupBox1.ForeColor = System.Drawing.Color.White;
             this.groupBox1.Location = new System.Drawing.Point(13, 33);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(807, 287);
+            this.groupBox1.Size = new System.Drawing.Size(807, 300);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "DATOS DEL TUTOR";
@@ -110,7 +114,7 @@
             // button_Buscar
             // 
             this.button_Buscar.ForeColor = System.Drawing.Color.Black;
-            this.button_Buscar.Location = new System.Drawing.Point(318, 214);
+            this.button_Buscar.Location = new System.Drawing.Point(314, 237);
             this.button_Buscar.Name = "button_Buscar";
             this.button_Buscar.Size = new System.Drawing.Size(103, 36);
             this.button_Buscar.TabIndex = 14;
@@ -120,7 +124,7 @@
             // 
             // numericUpDown_CapacidadMaxima
             // 
-            this.numericUpDown_CapacidadMaxima.Location = new System.Drawing.Point(239, 153);
+            this.numericUpDown_CapacidadMaxima.Location = new System.Drawing.Point(239, 183);
             this.numericUpDown_CapacidadMaxima.Name = "numericUpDown_CapacidadMaxima";
             this.numericUpDown_CapacidadMaxima.Size = new System.Drawing.Size(133, 29);
             this.numericUpDown_CapacidadMaxima.TabIndex = 13;
@@ -128,7 +132,7 @@
             // button_RegistrarTutor
             // 
             this.button_RegistrarTutor.ForeColor = System.Drawing.Color.Black;
-            this.button_RegistrarTutor.Location = new System.Drawing.Point(147, 214);
+            this.button_RegistrarTutor.Location = new System.Drawing.Point(147, 237);
             this.button_RegistrarTutor.Name = "button_RegistrarTutor";
             this.button_RegistrarTutor.Size = new System.Drawing.Size(133, 36);
             this.button_RegistrarTutor.TabIndex = 7;
@@ -140,7 +144,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(30, 153);
+            this.label4.Location = new System.Drawing.Point(30, 183);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(144, 21);
             this.label4.TabIndex = 6;
@@ -148,7 +152,7 @@
             // 
             // textBox_NombreTutor
             // 
-            this.textBox_NombreTutor.Location = new System.Drawing.Point(239, 96);
+            this.textBox_NombreTutor.Location = new System.Drawing.Point(239, 88);
             this.textBox_NombreTutor.Name = "textBox_NombreTutor";
             this.textBox_NombreTutor.Size = new System.Drawing.Size(213, 29);
             this.textBox_NombreTutor.TabIndex = 4;
@@ -167,11 +171,11 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Maiandra GD", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(30, 102);
+            this.label3.Location = new System.Drawing.Point(30, 96);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(189, 21);
+            this.label3.Size = new System.Drawing.Size(93, 21);
             this.label3.TabIndex = 2;
-            this.label3.Text = "NOMBRE Y APELLIDO";
+            this.label3.Text = "NOMBRE \r\n";
             // 
             // label2
             // 
@@ -179,7 +183,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Maiandra GD", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(30, 42);
+            this.label2.Location = new System.Drawing.Point(34, 50);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 21);
             this.label2.TabIndex = 1;
@@ -210,12 +214,28 @@
             this.groupBox2.Controls.Add(this.dataGridView_Tutor);
             this.groupBox2.Font = new System.Drawing.Font("Maiandra GD", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.ForeColor = System.Drawing.Color.Black;
-            this.groupBox2.Location = new System.Drawing.Point(13, 348);
+            this.groupBox2.Location = new System.Drawing.Point(13, 357);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(821, 383);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "LISTA DE TUTORES REGISTRADOS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 21);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "APELLIDO";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(239, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 29);
+            this.textBox1.TabIndex = 19;
             // 
             // Form_Tutor
             // 
@@ -257,5 +277,7 @@
         private System.Windows.Forms.TextBox textBox_Buscar;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
     }
 }
